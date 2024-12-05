3458d35612673436249ce3d4fd43ca90ec0d68d8
Matthew Denton <mattdenton94@gmail.com>
2024-12-05 21:09:50 +0200

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end
 
   resources :bookmarks, only: [:destroy]
-  
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  root "lists#index"
   # Defines the root path route ("/")
   # root "posts#index"
 end
